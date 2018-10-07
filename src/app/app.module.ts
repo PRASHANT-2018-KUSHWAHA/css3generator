534eee8ee7eeaf2c22551256a633b6d156dae44e
PRASHANT-2018-KUSHWAHA <prashantkushwaha000@gmail.com>
2018-10-07 20:16:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BoxshadowComponent } from './boxshadow/boxshadow.component';
+import { GradientComponent } from './gradient/gradient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BoxshadowComponent,
+    GradientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/gradient/gradient.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gradient',
+  templateUrl: './gradient.component.html',
+  styleUrls: ['./gradient.component.css']
+})
+export class GradientComponent implements OnInit {
+
+  color1 = 'yellow';
+  color2 = 'blue';
+  direction = 'to right';
+  id = 1;
+  selectMe(id) {
+    this.id = id;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
